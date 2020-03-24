8dc99a4e0e44b718c1e209f48f451cc1675b3bba
alyzidan <alyzidan.android@gmail.com>
2020-03-24 14:15:11 +0200

    initial commit and completing the task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./App.css";
+import data from "./data";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [],
+      selected: false
+    };
+  }
+  sortData(event) {
+    const value = event.target.value;
+    this.setState(
+      {
+        persons: data
+      },
+      this.sorting(value)
+    );
+  }
+  sorting(value) {
+    const persons = this.state.persons;
+    if (value === "name") {
+      persons.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (value === "age") {
+      persons.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    this.setState({
+      persons
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className={"container"}>
+          <h1 className={"text-center"}>birthday Recodrds</h1>
+          <form
+            className={"form-inline forms"}
+            onChange={this.sortData.bind(this)}
+          >
+            <div class="form-group">
+              <label>
+                <input type="radio" value="name" name="sorting" /> Sort by name
+              </label>
+            </div>
+            <div class="form-group">
+              <label>
+                <input type="radio" value="age" name="sorting" /> Sort by age
+              </label>
+            </div>
+          </form>
+          <table className={"table table-striped"}>
+            <tbody>
+              <tr>
+                <th>person name</th>
+                <th>date of birth</th>
+              </tr>
+              {this.state.persons.length !== 0 ? (
+                this.state.persons.map(person => (
+                  <tr key={person.index}>
+                    <td>{person.name}</td>
+                    <td>{person.date}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <p className="text-center text-info">
+                    Kindly select radio buttons to load the data
+                  </p>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = [{
+      "index": 0,
+      "name": 'Alexander Alfred',
+      "date": '02/09/1891',
+    },
+    {
+      "index": 1,
+      "name": 'Cecillia Olsson',
+      "date": '09/16/1992',
+    },
+    {
+      "index": 3,
+      "name": 'Debreon T.decker',
+      "date": '10/31/1999',
+    },
+    {
+      "index": 2,
+      "name": 'Janice shroyer',
+      "date": '12/01/1982',
+    },
+    {
+      "index": 4,
+      "name": 'Jimmy Shergie',
+      "date": '12/122001',
+    },
+    {
+      "index": 5,
+      "name": 'Peter Parker',
+      "date": '01/16/1992',
+    },
+    {
+      "index": 6,
+      "name": 'Ralph white',
+      "date": '11/30/2011',
+    },
+    {
+      "index": 7,
+      "name": 'Veronica Mize',
+      "date": '11/30/2011',
+    }]
+    export default data;
+  
